29ee40d5eec8338127da3792bf175f805464c80f
crestrepo12 <CarolinaRestrepo@ac.c4q.nyc>
2018-05-04 12:02:51 -0400

    make it better

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ app.use("/users", users);
 
 //place this after backend routing
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
   });
 
 // catch 404 and forward to error handler
