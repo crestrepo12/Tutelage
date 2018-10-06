d13ac0837c64592606aed3a1a7765d4e79d6208f
crestrepo12 <CarolinaRestrepo@ac.c4q.nyc>
2018-10-06 19:23:02 -0400

    fixed the footer especially not to overlap login/register form

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,37 +1,33 @@
-import React, { Component } from 'react';
-import '../css/Footer.css';
-
-
-
+import React, { Component } from "react";
+import "../css/Footer.css";
 
 class Footer extends Component {
-    constructor() {
-        super();
-    }
+  render() {
+    return (
+      <footer>
+        <hr />
+        <ul>
+          <p id="copyright">
+            Tutelage Inc.&trade; Copyright © 2018 Until Infinity
+          </p>
+          <p id="rights">All Rights Reserved</p>
+          <p className="hqSection">
+            527 Innovation Parkway, Suite 7, New York, NY 10001
+          </p>
+        </ul>
 
-    render() {
-        return (
-                    <span className="footerSection">
-                        <span id="copyright">Tutelage Inc.&trade; Copyright © 2018 Until Infinity</span>
-                        <span id="rights">All Rights Reserved</span>
-                        <span className="hqSection">
-                            <span>527 Innovation Parkway, Suite 7, New York, NY 10001</span>
-                        </span>
-                        <div className="linkSection">
-                            <div> 
-                                <a href="https://github.com/Acostill/Tutelage">
-                                    <img id="githubLogo" src="./images/githubplainlogo.svg" alt="Github Repository"/>
-                                </a>
-                            </div>
+        <a href="https://github.com/Acostill/Tutelage">
+          <img
+            id="githubLogo"
+            src="./images/githubplainlogo.svg"
+            alt="Github Repository"
+          />
+        </a>
 
-                        </div>
-                        <div id="tagline">Reach out to your full potential.&trade;</div>
-
-                    </span>
-        )
-    }
-
+        <p id="tagline">Reach out to your full potential.&trade;</p>
+      </footer>
+    );
+  }
 }
-
 
 export default Footer;
